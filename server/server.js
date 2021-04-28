bd1ad895e89502fdd65d32e682b92b352aec9d48
nicolas.orellana <nicolas.orellana@ibm.com>
2021-04-27 21:32:33 -0400

    updated json

--- server/server.js
+++ server/server.js
@@ -1,16 +1,12 @@
-const express = require('express')
-const apiRouter = require('./routes')
+const express = require("express");
+const apiRouter = require("./routes");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api/artist', apiRouter)
+app.use("/api/artist", apiRouter);
 
-app.listen(process.env.PORT || '3000', () => {
-
-    console.log("Listening!")
-    
-
+app.listen(process.env.PORT || "3000", () => {
+  console.log("Listening!");
 });
-
